075905ca9780b625f2b9860b1468030c65133a1f
dnlouko <danbnascimento@gmail.com>
2025-11-25 20:35:01 -0300

    Add populateAvatarSelector call to initApp

--- web/js/main.js
+++ web/js/main.js
@@ -11,6 +11,9 @@ function initApp() {
         requestNotificationPermission();
     }
 
+    // Populate avatar selector
+    populateAvatarSelector();
+
     initWebSocket();
 
     if (!window.currentUser) {
